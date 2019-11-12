b976c0349a56ef080cf6d87b97da8fee3685d735
Schittaci-air <barak.simon.as@gmail.com>
2019-11-12 21:56:30 +0100

    create map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,47 @@
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
+// UTIL FUNCTION
+// Get Coordinates in array format
+const getCoordinates = el => {
+  const coordinatesLon = Number(el.getAttribute("data-lon"));
+  const coordinatesLat = Number(el.getAttribute("data-lat"));
+  return [coordinatesLon, coordinatesLat];
+};
+
+const createGeoJason = (el, index) => {
+  // get mark coordinates
+  const coordinates = getCoordinates(el);
+
+  const completedObject = {
+    index: index,
+    geometry: {
+      coordinates: coordinates
+    }
+  };
+  return completedObject;
+};
+
+// geojson.forEach(marker => {
+//   // create a DOM element for the marker
+//   var el = document.createElement("div");
+//   el.innerHTML = `A`;
+//   el.className = "w-2 h-3 bg-gray-900";
+
+//   var mapEntryObj = {
+//     element: el,
+//     index: marker.index + 1,
+
+//     flyTo: {
+//       coordinates: marker.geometry.coordinates,
+//       zoom: 16.6,
+//       speed: 1.5
+//     }
+//   };
+
+//   // add marker to map
+//   new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+// });
+
 export const buildMap = () => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoibG9uZXJzY29tcGFueSIsImEiOiJjandqNTlsYXIwNjduNDRwNjhkemhhZXVtIn0.c2PlH4OqBZUOQgDM4hg7fw";
@@ -11,4 +53,22 @@ export const buildMap = () => {
   });
 
   map.scrollZoom.disable();
+
+  const locationEls = Array.from(
+    document.getElementsByClassName("map-location")
+  );
+
+  const geojson = locationEls.map(createGeoJason);
+
+  geojson.forEach(marker => {
+    //addMapEvent(mapEntryObj);
+
+    var el = document.createElement("div");
+    el.className = "h-4 w-4 bg-blue-600 rounded-full";
+
+    // add marker to map
+    new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
+  });
+
+  console.log(geojson);
 };
