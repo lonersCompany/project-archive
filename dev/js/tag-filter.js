fb766772cc38101495898f80e7bd9ece9e371650
Schittaci-air <barak.simon.as@gmail.com>
2019-12-30 16:16:27 +0100

    change gellery cards

--- dev/js/tag-filter.js
+++ dev/js/tag-filter.js
@@ -1,39 +1,16 @@
 export const initTagFilters = mainEl => {
-  // const toggleButton = (params) => {
-
-  // }
-
-  // What is data structure?
-
-  // const appStructure = {
-  //     filters: [
-  //         "Blue", "Green", "Red"
-  //     ],
-  //     entries: [
-  //         {
-  //             name: "Project 1"
-  //             tags: ["Blue", "Green"]
-  //         },
-  //         {
-  //             name: "Project 2"
-  //             tags: ["Green", "Red"]
-  //         },
-
-  //         {
-  //             name: "Project 2"
-  //             tags: ["Red", "Blue"]
-  //         },
-  //     ]
-  // }
-
   const btnEls = Array.from(mainEl.querySelectorAll(".btn-filter"));
+  const attributesArr = btnEls.map(el => el.getAttribute("data-tag"));
 
   btnEls.forEach(el => {
     const value = el.getAttribute("data-tag");
 
     el.addEventListener("click", () => {
-      el.classList.toggle("bg-gray-300");
-      mainEl.classList.toggle(value);
+      attributesArr.forEach(attribute => mainEl.classList.remove(attribute));
+      btnEls.forEach(el => el.classList.remove("bg-gray-300"));
+
+      el.classList.add("bg-gray-300");
+      mainEl.classList.add(value);
     });
   });
 };
